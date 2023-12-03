8e85028cfcaa7396e0a63b14276914b34ab170f3
Norbi0801 <norbertolkowski@gmail.com>
2023-12-03 08:47:51 +0100

    Add getWorkingDirectory

---
+++ src/Utilities/Utilities.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fmt/format.h>
+
+namespace Utils {
+    inline std::string GetWorkingDirectory() {
+        std::filesystem::path workingDir = std::filesystem::current_path();
+        char preferred_separator = std::filesystem::path::preferred_separator;
+        return fmt::format("{}{}", workingDir.string(), preferred_separator);
+    }
+}
