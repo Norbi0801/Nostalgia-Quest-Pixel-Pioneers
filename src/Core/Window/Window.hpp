1259e20695bf8d96d706f20772fcb3f2f3b2f5d2
Norbi0801 <norbertolkowski@gmail.com>
2023-12-03 09:15:04 +0100

    add basic Window class

---
+++ src/Core/Window/Window.hpp
@@ -0,0 +1,36 @@
+//
+// Created by Norbert Olkowski on 03.12.2023.
+//
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include <string>
+
+namespace Window {
+
+    class Window{
+    public:
+        Window();
+        Window(const std::string& l_title,const sf::Vector2u& l_size);
+        ~Window();
+        void BeginDraw(); // Clear the window.
+        void EndDraw(); // Display the changes.
+        void Update();
+        bool IsDone() const;
+        bool IsFullscreen() const;
+        sf::Vector2u GetWindowSize();
+        void ToggleFullscreen();
+        void Draw(sf::Drawable& l_drawable);
+
+    private:
+        void Setup(const std::string& l_title, const sf::Vector2u& l_size);
+        void Destroy();
+        void Create();
+        sf::RenderWindow m_window;
+        sf::Vector2u m_windowSize;
+        std::string m_windowTitle;
+        bool m_isDone{};
+        bool m_isFullscreen{};
+    };
+
+}
