a1b319f6d17e6e6af44f985b92c30381b806acd4
Norbi0801 <norbertolkowski@gmail.com>
2023-12-08 08:56:01 +0100

    split Tile to cpp hpp

--- src/Map/Tile/Tile.cpp
+++ src/Map/Tile/Tile.cpp
@@ -1,3 +1,32 @@
 //
 // Created by Norbert Olkowski on 08.12.2023.
 //
+
+#include "Tile.hpp"
+
+namespace Tile {
+
+    TileInfo::TileInfo(State::SharedContext *l_context, const std::string &l_texture, TileID l_id)
+            : m_context(l_context), m_id(0), m_deadly(false) {
+        Texture::TextureManager *tmgr = l_context->m_textureManager;
+        if (l_texture.empty()) {
+            m_id = l_id;
+            return;
+        }
+        if (!tmgr->RequireResource(l_texture)) { return; }
+        m_texture = l_texture;
+        m_id = l_id;
+        m_sprite.setTexture(*tmgr->GetResource(m_texture));
+        sf::IntRect tileBoundaries(m_id %
+                                   (Sheet::Sheet_Width / Sheet::Tile_Size) * Sheet::Tile_Size,
+                                   m_id / (Sheet::Sheet_Height / Sheet::Tile_Size) * Sheet::Tile_Size,
+                                   Sheet::Tile_Size, Sheet::Tile_Size);
+        m_sprite.setTextureRect(tileBoundaries);
+    }
+
+    TileInfo::~TileInfo() {
+        if (m_texture.empty()) { return; }
+        m_context->m_textureManager->ReleaseResource(m_texture);
+    }
+
+}
