8f2b76294b967782a3f967a7e5cdf8dde6adbff3
Norbi0801 <norbertolkowski@gmail.com>
2023-12-12 19:27:27 +0100

    add EventQueue.hpp

---
+++ src/Utilities/EventQueue.hpp
@@ -0,0 +1,27 @@
+//
+// Created by Norbert Olkowski on 12.12.2023.
+//
+
+#pragma once
+
+#include <queue>
+
+using EventID = unsigned int;
+
+
+class EventQueue {
+public:
+    void AddEvent(const EventID &l_event) { m_queue.push(l_event); }
+
+    bool ProcessEvents(EventID &l_id) {
+        if (m_queue.empty()) { return false; }
+        l_id = m_queue.front();
+        m_queue.pop();
+        return true;
+    }
+
+    void Clear() { while (!m_queue.empty()) { m_queue.pop(); }}
+
+private:
+    std::queue<EventID> m_queue;
+};
