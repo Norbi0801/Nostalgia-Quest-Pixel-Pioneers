56b741eaf02894afea9ef88e74ac7fe494a9985c
Norbi0801 <norbertolkowski@gmail.com>
2023-12-12 19:24:42 +0100

    add ECS.hpp

---
+++ src/Core/ECS/ECS.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Norbert Olkowski on 12.12.2023.
+//
+
+#pragma once
+
+namespace ECS {
+
+    using ComponentType = unsigned int;
+#define N_COMPONENT_TYPES 32
+    enum class Component {
+        Position = 0, SpriteSheet, State, Movable, Controller, Collidable
+    };
+    enum class System {
+        Renderer = 0, Movement, Collision, Control, State, SheetAnimation
+    };
+
+    enum class EntityEvent {
+        Spawned, Despawned, Colliding_X, Colliding_Y,
+        Moving_Left, Moving_Right, Moving_Up, Moving_Down,
+        Elevation_Change, Became_Idle, Began_Moving
+    };
+
+}
