c9ab37db5beb4ce7436f76b8b577fbba8fb964e8
Norbi0801 <norbertolkowski@gmail.com>
2023-12-26 19:59:33 +0100

    add StateMainMenu

---
+++ src/States/State_MainMenu/State_MainMenu.hpp
@@ -0,0 +1,34 @@
+//
+// Created by Norbert Olkowski on 26.12.2023.
+//
+
+#pragma once
+
+#include "Core/State/State.hpp"
+
+namespace State {
+    class State_MainMenu : public BaseState{
+    public:
+        void OnCreate() override;
+
+        void OnDestroy();
+
+        void Activate();
+
+        void Deactivate();
+
+        void Update(const sf::Time &l_time);
+
+        void Draw();
+
+        void MouseClick(Event::EventDetails* l_details);
+    private:
+        sf::Text m_text;
+        sf::Font m_font;
+        sf::Vector2f m_buttonSize;
+        sf::Vector2f m_buttonPos;
+        unsigned int m_buttonPadding;
+        sf::RectangleShape m_rects[3];
+        sf::Text m_labels[3];
+    };
+}
