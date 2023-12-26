578b4ad011506af8a6875b1c97e73f359d60fecd
Norbi0801 <norbertolkowski@gmail.com>
2023-12-26 19:59:18 +0100

    add StatePaused

---
+++ src/States/State_Paused/State_Paused.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Norbert Olkowski on 26.12.2023.
+//
+
+#include "State_Paused.hpp"
+
+namespace State{
+    void State_Paused::OnCreate(){
+        SetTransparent(true); // Set our transparency flag.
+        m_font.loadFromFile("arial.ttf");
+        m_text.setFont(m_font);
+        m_text.setString(sf::String("PAUSED"));
+        m_text.setCharacterSize(14);
+        m_text.setStyle(sf::Text::Bold);
+        sf::Vector2u windowSize = m_stateMgr->
+                GetContext()->m_wind->GetRenderWindow().getSize();
+        sf::FloatRect textRect = m_text.getLocalBounds();
+        m_text.setOrigin(textRect.left + textRect.width / 2.0f,
+                         textRect.top + textRect.height / 2.0f);
+        m_text.setPosition(windowSize.x / 2.0f, windowSize.y / 2.0f);
+        m_rect.setSize(sf::Vector2f(windowSize));
+        m_rect.setPosition(0,0);
+        m_rect.setFillColor(sf::Color(0,0,0,150));
+        Event::EventManager* evMgr = m_stateMgr->
+                GetContext()->m_eventManager;
+        evMgr->AddCallback(StateType::Paused,"Key_P",
+                           &State_Paused::Unpause,this);
+    }
+
+    void State_Paused::OnDestroy(){
+        Event::EventManager* evMgr = m_stateMgr->
+                GetContext()->m_eventManager;
+        evMgr->RemoveCallback(StateType::Paused,"Key_P");
+    }
+
+    void State_Paused::Draw(){
+        sf::RenderWindow& wind = m_stateMgr->
+                GetContext()->m_wind->GetRenderWindow();
+        wind.draw(m_rect);
+        wind.draw(m_text);
+    }
+
+    void State_Paused::Unpause(Event::EventDetails* l_details){
+        m_stateMgr->SwitchTo(StateType::Game);
+    }
+}
---
+++ src/States/State_Paused/State_Paused.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Norbert Olkowski on 26.12.2023.
+//
+
+#pragma once
+
+#include "Core/State/State.hpp"
+
+namespace State {
+    class State_Paused : public BaseState{
+    public:
+        void OnCreate() override;
+
+        void OnDestroy();
+
+        void Activate();
+
+        void Deactivate();
+
+        void Update(const sf::Time &l_time);
+
+        void Draw();
+        void Unpause(Event::EventDetails* l_details);
+    private:
+        sf::Text m_text;
+        sf::Font m_font;
+        sf::RectangleShape m_rect;
+    };
+}
